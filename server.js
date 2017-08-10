e43f1048fff31ab58282c6ce6e0cd12793832d99
Bruce Graham <brucegraham11@gmail.com>
2017-08-09 22:24:41 -0700

    Add logic to the subrangesSum function

--- server.js
+++ server.js
@@ -2,10 +2,33 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-const sumSubranges = (n, k, averages) => {
-  console.log(n);
-  console.log(k);
-  console.log(averages);
+const subrangesSum = (n, k, averages) => {
+  let avgs = averages.slice();
+
+  const subranges = arr => {
+    let total = 0;
+
+    while(arr.length >= k) {
+      for(var i = 0; i < arr.length; i++) {
+        const index0 = arr[0];
+        const indexI = arr[i];
+
+        if (index0 < indexI) {
+          total++;
+        } else if (index0 > indexI) {
+          total--;
+        }
+        arr.shift();
+      }
+    }
+    console.log(total);
+    avgs.shift();
+    if (avgs.length >= k) {
+      subranges(avgs.slice(0, k));
+    }
+  }
+
+  subranges(avgs.slice(0, k));
 };
 
 fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
@@ -16,7 +39,7 @@ fs.readFile(__dirname + '/input.txt', 'utf8', (err, data) => {
     const n = content.shift();
     const k = content.shift();
 
-    sumSubranges(n, k, content);
+    subrangesSum(n, k, content);
   }
 });
 
